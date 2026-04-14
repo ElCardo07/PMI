6a4f2040c73926a6d63facc1b2c59c9890078c10
Netbook <Netbook@DESKTOP-LDADRIF>
2026-04-14 19:53:01 -0300

    nada

---
+++ Libro/src/libro/Libro.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package libro;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Netbook
+ */
+public class Libro {
+private int isbn;
+private String nombre;
+private String autor;
+private int numpag;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
